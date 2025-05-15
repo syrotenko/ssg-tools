d03fc4c8039c81f0e9ec2197e6cc6ff6ddfb0323
Serhii Syrotenko <ssyrotenko@amcbridge.com>
2025-05-15 15:54:03 +0300

    Suppress any output from git command

--- fetchAll.ps1
+++ fetchAll.ps1
@@ -1,18 +1,18 @@
 # Go through all subdirectories in the current directory and run git fetch in each one
- 
+
 # Get the current directory
 $currentDir = Get-Location
- 
+
 # Get all subdirectories in the current directory
 $subDirs = Get-ChildItem -Directory
- 
+
 # Count the number of subdirectories
 $totalDirs = $subDirs.Count
 $currentDirIndex = 0
- 
+
 # Create a collection to store job information
 $jobs = @()
- 
+
 # Loop through each subdirectory and start a job to run git fetch
 foreach ($dir in $subDirs) {
     $currentDirIndex++
@@ -24,10 +24,17 @@ foreach ($dir in $subDirs) {
 
         # Git fetch outputs to stderr, which PowerShell treats as an error
         # redirect the stderr output of the git fetch command to stdout
-        git fetch 2>&1
+        # Even when the output is redirected, PowerShell may still log those messages as errors
+        try {
+            $output = git fetch 2>&1
+            # Write-Output $output
+        } catch {
+            # suppress for now
+            # Write-Output "Error fetching in directory $dirPath: $_"
+        }
     } -ArgumentList $dir.FullName, $currentDirIndex, $totalDirs
     $jobs += $job
 }
- 
+
 # Wait for all jobs to complete
 $jobs | ForEach-Object { Receive-Job -Job $_ -Wait }
