55a82eb36ff1f942ec954d54b8c1d30e1bdb244a
Serhii Syrotenko <ssyrotenko@amcbridge.com>
2025-05-15 12:28:16 +0300

    improve fetchAll to show persanteage progress

--- fetchAll.ps1
+++ fetchAll.ps1
@@ -1,27 +1,33 @@
 # Go through all subdirectories in the current directory and run git fetch in each one
-
+ 
 # Get the current directory
 $currentDir = Get-Location
-
+ 
 # Get all subdirectories in the current directory
 $subDirs = Get-ChildItem -Directory
-
+ 
+# Count the number of subdirectories
+$totalDirs = $subDirs.Count
+$currentDirIndex = 0
+ 
 # Create a collection to store job information
 $jobs = @()
-
+ 
 # Loop through each subdirectory and start a job to run git fetch
 foreach ($dir in $subDirs) {
+    $currentDirIndex++
     $job = Start-Job -ScriptBlock {
-        param($dirPath)
-        Write-Output "Fetching in directory: $dirPath"
+        param($dirPath, $currentDirIndex, $totalDirs)
+        $percentage = [math]::Round(($currentDirIndex / $totalDirs) * 100, 0)
+        Write-Output "($percentage%) Fetching in directory $dirPath"
         Set-Location $dirPath
 
         # Git fetch outputs to stderr, which PowerShell treats as an error
         # redirect the stderr output of the git fetch command to stdout
         git fetch 2>&1
-    } -ArgumentList $dir.FullName
+    } -ArgumentList $dir.FullName, $currentDirIndex, $totalDirs
     $jobs += $job
 }
-
+ 
 # Wait for all jobs to complete
 $jobs | ForEach-Object { Receive-Job -Job $_ -Wait }
